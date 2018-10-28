e73ac6bdd3df306c41f933241a0cf880e4ad6470
Steve Mustanski <steve.mustanski@gmail.com>
2018-10-27 20:13:40 -0400

    added methods for sorting and voting to models.js

--- models.js
+++ models.js
@@ -4,22 +4,40 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// sort function to sort anwers properly
+let sortAnswers = function (a, b) {
+  // negative if a before b
+  // 0 no change
+  // postive if a after b
+  if (a.votes === b.votes) {
+      return b.updatedAt - a.updatedAt;
+    } 
+  return b.votes - a.votes
+}
+
 // Set up the answer Schema
-let AnswerSchema = new Schema ({
+let AnswerSchema = new Schema({
   text: String,
-  createdAt: {type: Date, default: Date.now},
-  updatedAt: {type: Date, default: Date.now},
-  votes: {type: Number, default:0}
-})
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  votes: { type: Number, default: 0 }
+});
 
 // Set up the Question Schema
-let QuestionSchema = new Schema ({
+let QuestionSchema = new Schema({
   text: String,
-  createdAt: {type: Date, default: Date.now},
+  createdAt: { type: Date, default: Date.now },
   answers: [AnswerSchema]
+});
+
+// pre save callback to have mongoose sort entries prior to saving
+QuestionSchema.pre('save', (next) => {
+  // pass in sort method to tell js how to sort the document
+  this.answers.sort(sortAnswers);
+  next();
 })
 
-//
+
 let Question = mongoose.model('Question', QuestionSchema);
 
 model.exports.Question = Question;
